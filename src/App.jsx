4b01dced61b7a1e7b08b4e3208e1d3e9635f1f6c
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2024-05-16 17:05:00 -0500

    add about.jsx the language 'es' or 'en'

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,42 @@
+import { Suspense, lazy } from 'react'
 import './App.css'
-import HomePage from './pages/Home.jsx'
-import AboutPage from './pages/About.jsx'
+// import HomePage from './pages/Home.jsx'
+// import AboutPage from './pages/About.jsx' // import estatico
 import Page404 from './pages/404.jsx'
 import { Router } from './components/Router.jsx'
 import SearchPage from './pages/Search.jsx'
 import { Route } from './components/Route.jsx'
 
-//import { navigate } from './components/Link.jsx'
+// esto es un import dinamico, solo se carga cuando se ejecuta
+
+const LazyHomePage = lazy(() => import('./pages/Home.jsx'))
+const LazyAboutPage = lazy(() => import('./pages/About.jsx'))
+
 //se paso el navigate a Link.jsx
+// /search/javascript /search/python, etc
 const appRoutes = [
-{
-  path: '/search/:query',   // /search/javascript /search/python, etc
-  Component: SearchPage
-}
+  {
+    path: '/:lang/about',
+    Component: LazyAboutPage
+  },
+  {
+    path: '/search/:query',
+    Component: SearchPage
+  }
 ]
 
 function App() {
   return (
     <main>
-      <h3>react router</h3>
-      {/* esto es renderizado condicional, clasico de react */}
-      <Router routes={appRoutes} defaultComponent={Page404}>
-        <Route path='/' Component={HomePage} />
-        <Route path='/about' Component={AboutPage} />
-      </Router>
+      {/* se puede colocar el fallback={null} */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <h3>react router</h3>
+        {/* esto es renderizado condicional, clasico de react */}
+        <Router routes={appRoutes} defaultComponent={Page404}>
+          <Route path='/' Component={LazyHomePage} />
+          <Route path='/about' Component={LazyAboutPage} />
+        </Router>
+      </Suspense>
     </main>
   )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,35 @@
-import { navigate } from "../components/Link" 
-export default function AboutPage(){
-    return(
-      <>
-        <h1>About</h1>
-        <div>
-          <img src="https://media.licdn.com/dms/image/D4E03AQFbpgbYNeP4ig/profile-displayphoto-shrink_200_200/0/1684341532395?e=1721260800&v=beta&t=IdOzLUOiZlHuULFFqGpXFY9ShWjTJrhlKjX-dnAg7ug" alt="raa" />
-          <p>Estoy creando un clone de react router </p>
-        </div>
-        {/* <a href='/'>Ir al Home</a> */}
-        <button onClick={()=>navigate('/')}>Ir al Home</button>
-      </>
-    )
+import { Link } from "../components/Link"
+
+const i18n = {
+  es: {
+    title: 'Sobre nosotros',
+    buttom: 'Ir a la home',
+    description: 'Hola me llamo Rene, creando un clone de react router'
+  },
+  en: {
+    title: 'About is',
+    buttom: 'Go to home page',
+    description: 'Hi! my name is Rene, I am creating a clone of react router'
   }
+}
+
+const useI18n = (lang) =>{
+  return i18n[lang] || i18n.en
+}
+
+export default function AboutPage({ routeParams }) {
+  const i18n = useI18n(routeParams.lang ?? 'es')
+
+  return (
+    <>
+      <h1>{i18n.title}</h1>
+      <div>
+        <img src="https://media.licdn.com/dms/image/D4E03AQFbpgbYNeP4ig/profile-displayphoto-shrink_200_200/0/1684341532395?e=1721260800&v=beta&t=IdOzLUOiZlHuULFFqGpXFY9ShWjTJrhlKjX-dnAg7ug" alt="raa" />
+        <p>{i18n.description}</p>
+      </div>
+      {/* <a href='/'>Ir al Home</a> */}
+      <Link to='/'>{i18n.buttom}</Link>
+      {/* <button onClick={() => navigate('/')}>Ir al Home</button> */}
+    </>
+  )
+}
